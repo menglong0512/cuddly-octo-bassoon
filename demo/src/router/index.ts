f87a68676e3dacd4221e52b0b38d3489ad92789f
王弘毅 <>
2018-12-06 16:03:22 +0800

    添加路由懒加载

--- demo/src/router/index.ts
+++ demo/src/router/index.ts
@@ -1,14 +1,14 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import indexView from '@/components/index.vue'
+// import indexView from '@/components/index.vue'
 
 Vue.use(Router)
 
 const router = new Router({
   routes: [
-    // {path: '/', name: 'login', component: () => require('@/components/login.vue')},
-    {path: '/index', name: 'index', component: require('@/components/index.vue').default, children:[
-      {path: 'indexView_1', name: 'indexView_1', component: require('@/components/indexView/one.vue').default}
+    {path: '/', name: 'login', component: () => import('@/components/login.vue')},
+    {path: '/index', name: 'index', component: () => import('@/components/index.vue'), children: [
+      {path: 'nest_1', name: 'nest_1', component: () => import('@/components/indexView/nest.vue')}
     ]}
   ]
 })
