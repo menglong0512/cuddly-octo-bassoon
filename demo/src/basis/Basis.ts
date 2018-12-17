c9d635178eed4ed82c188877c7c8682ba7ab26f1
王弘毅 <>
2018-12-17 17:16:15 +0800

    添加axios请求

--- demo/src/basis/Basis.ts
+++ demo/src/basis/Basis.ts
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Component from 'vue-class-component'
 import {Prop, Watch, Emit} from 'vue-property-decorator'
+import axios from 'axios'
 
 /**
  * 基类用来定义共用方法
@@ -9,10 +10,57 @@ import {Prop, Watch, Emit} from 'vue-property-decorator'
  * @extends {Vue}
  */
 export class Basis extends Vue{
-    get(apiUrl: string, query?: object){
-        
-        // return {} 
+
+    /**
+     * 请求地址
+     */
+    siteServer: string = '';
+
+    /**
+     * ajax请求 -- axios
+     * @param method 请求方式
+     * @param apiUrl 请求地址
+     * @param queryData 请求参数 
+     */
+    SEQAjax(method: string, apiUrl: string, queryData?: object): Promise<any> {
+
+        return new Promise(async (resolve, reject) => {
+            const resovleHandler = async (rsp:any) => {
+                let {message = '操作成功', result = true, status = 200} = rsp
+                if (result && status !== 200) result = false
+                if (result === 400) message = '请求不存在'
+                if (result || !rsp.result) {
+                  if (!rsp.result) {
+                    // Message({message, type: 'error', duration: '2000'})
+                  }
+                }
+                resolve(rsp) // 在异步操作成功时调用，并将异步操作的结果作为参数传递出去
+            }
+
+            try {
+                axios({
+                    method: method,
+                    headers: {'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'},
+                    url: `${this.siteServer}${apiUrl}`,
+                    timeout: 30000,
+                    data: queryData
+                }).then(response => {
+                    const status = response.status
+                    try {
+                        resovleHandler({...response.data, status})
+                    } catch (e) {
+                        resovleHandler({status, success: false, message: '网络错误'})
+                    }
+                });
+            } catch (error) {
+                resovleHandler({status: 404, success: false, message: '网络错误'})
+            }
+        })
+
     }
+
+    myfecth(){}
+    
 }
 
 export {Component, Prop, Watch, Emit}
--- demo/src/router/index.ts
+++ demo/src/router/index.ts
@@ -6,7 +6,8 @@ Vue.use(Router)
 
 const router = new Router({
   routes: [
-    {path: '/', name: 'login', component: () => import('@/components/login.vue')},
+    // {path: '/', name: 'login', component: () => import('@/components/login.vue')},
+    {path: '/', name: 'login', component: resolve => require(['@/components/login.vue'], resolve)},
     {path: '/index', name: 'index', component: () => import('@/components/index.vue'), children: [
       {path: 'nest_1', name: 'nest_1', component: () => import('@/components/indexView/nest.vue')}
     ]},
